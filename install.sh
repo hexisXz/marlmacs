bdcaf8cfb8b0e96a648e84724efd6bb7705e536a
hexis <hexis@Debian.example.com>
2023-03-31 16:19:10 -0400

    updated the install.sh.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-mv init.el ~/.emacs.d/ && mv init.el.org ~/.emacs.d/ && echo "Done." && echo "to configure hexmacs edit the init.el.org."
+mv emacs3.png ~/Downloads && mv init.el ~/.emacs.d/ && mv init.el.org ~/.emacs.d/ && echo "Done." && echo "to configure hexmacs edit the init.el.org."
